3df41407f3b21e901119067d8db1b99ae0bf0054
Prince Goswami <prince@mrprincegoswami.in>
2024-10-03 19:19:57 +0530

    added username as required field in token model for easy identification of user

--- backend/model/tokenModel.js
+++ backend/model/tokenModel.js
@@ -6,6 +6,10 @@ const tokenSchema = mongoose.Schema({
         ref: 'User',
         required: true
     },
+    username:{
+        type:String,
+        required:true
+    },
     token: {
         type: String,
         required: true

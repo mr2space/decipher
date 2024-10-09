048e17ed14cedcc48f60e8e975a4c7339c98b2a2
Rahul Kushwaha <rahul1979kushwaha@gmail.com>
2024-10-09 12:42:46 +0530

    Completed Upload feature

--- backend/src/routes/image.upload.router.js
+++ backend/src/routes/image.upload.router.js
@@ -1,15 +1,7 @@
 import express from "express";
 import { upload } from "../config/multerConfig.js";
-const imageUploadRouter = express.Router();
+import {speciesPhotoHandler} from "../controllers/species.controller.js"
+const router = express.Router();
 
-imageUploadRouter.post('/upload', upload.single('photo'), (req, res) => {
-    if (!req.file) {
-      return res.status(400).send({ error: 'Please upload a file!' });
-    }
-    res.status(200).json({
-      message: 'File uploaded successfully',
-      url: req.file.location 
-    });
-  });
-
-export { imageUploadRouter };
+router.route("").post(upload.single('photo'), speciesPhotoHandler);
+export { router };

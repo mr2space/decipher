6a8404dd0f863a54b607944c621fe7ca2d64140a
Prince Goswami <prince@mrprincegoswami.in>
2024-10-10 07:45:38 +0530

    setup frontend

--- backend/src/routes/image.upload.router.js
+++ backend/src/routes/image.upload.router.js
@@ -2,6 +2,7 @@ import express from "express";
 import { upload } from "../config/multerConfig.js";
 import {speciesPhotoHandler} from "../controllers/species.controller.js"
 const router = express.Router();
+import { authenticate } from "../middleware/auth.middleware.js";
 
-router.route("").post(upload.single('photo'), speciesPhotoHandler);
+router.route("").post(authenticate, upload.single('photo'), speciesPhotoHandler);
 export { router };

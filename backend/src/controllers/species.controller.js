51a20c2723ff8696f1be200e8052db6450f3f2ae
Prince Goswami <71509870+mr2space@users.noreply.github.com>
2024-10-16 02:19:03 +0530

    added plantnet api for model

--- backend/src/controllers/species.controller.js
+++ backend/src/controllers/species.controller.js
@@ -6,7 +6,7 @@ import { User } from "../model/user.model.js";
 import { Species } from "../model/species.model.js";
 import axios from "axios";
 
-const saveLocation = async (position, name, user_id) =>{
+const saveLocation = async (position, name, user_id) => {
     if (
         (position[0] < -180 && position[0] > 180) ||
         (position[1] < -90 && position[1] > 90)
@@ -16,7 +16,6 @@ const saveLocation = async (position, name, user_id) =>{
             "invalid location[ longitude, latitude] field -180 <= longitude <= 180, -90 <=  latitude <= 90"
         );
     }
-
 
     let location = {
         type: "Point",
@@ -29,37 +28,56 @@ const saveLocation = async (position, name, user_id) =>{
     });
     await species.save();
     return species;
-}
+};
 
+const requestPlantSpecies = async (req, res) => {
+    const options = {
+        params: {
+            "include-related-images": true,
+            "nb-results": 1,
+            "api-key": process.env.MODEL_PLANT_API,
+            images: req.file.location,
+        },
+    };
+    const response = await axios.get(process.env.MODEL_PLANT_HOST, options);
+    console.log(JSON.stringify(response.data));
+    return {
+        score: response.data.results[0].score * 100,
+        species: [
+            response.data.results[0].species.scientificNameWithoutAuthor,
+            response.data.results[0].species?.commonNames[0],
+        ],
+        images: [
+            response.data.results[0].images[0].url.o,
+            response.data.results[0].images[0].url.m,
+        ],
+    };
+};
 
 const speciesPhotoHandler = asyncHandler(async (req, res) => {
     if (!req.file) {
         throw new ApiError(402, "file not upload successfully");
     }
-    
-    const options = {
-        headers: {
-            "Content-Type": "application/json",
-        },
-    };
-    const response = await axios.post(
-        "http://127.0.0.1:8000/type",
-        {
-            photo: req.file.location,
-        },
-        options
-    );
+    const details = await requestPlantSpecies(req, res);
     const payload = {
-        result:response.data,
-        location : {}
-    }
+        result: { ...details },
+        location: {},
+    };
     // logger.info(req.body);
     const position = req.body.location;
-    if (position){
-        logger.info(req.body.location)
-        payload.location = await saveLocation(position, payload.result?.species, req.user._id) // [longitude, latitude]
+    console.log(position);
+    if (position) {
+        logger.info(req.body.location);
+        payload.location = await saveLocation(
+            position,
+            payload.result?.species[1].toLowerCase(),
+            req.user._id
+        ); // [longitude, latitude]
+        payload.location.save();
     }
-    res.status(200).json(new ApiResponse(200, payload, "photo scan successfully"));
+    res.status(200).json(
+        new ApiResponse(200, payload, "photo scan successfully")
+    );
 });
 
 export { speciesPhotoHandler };

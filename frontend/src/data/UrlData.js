6c8f9884c435c932b90abf7181473827fc25e31d
Prince Goswami <71509870+mr2space@users.noreply.github.com>
2024-10-21 22:24:12 +0530

    shifted to post

--- frontend/src/data/UrlData.js
+++ frontend/src/data/UrlData.js
@@ -4,7 +4,7 @@ const URL = {
     HOME_URL : "/",
     BASE_URL : "http://localhost:7000",
     REFRESH_URL:"/auth/refresh-token",
-    SPECIES_SEARCH_URL : "/search/species",
+    SPECIES_SEARCH_URL : "/search",
     SPECIES_REACT_URL:"/search/result",
     MEDICINE_URL: "/search",
     LOCATION_SEARCH_URL : "/dev/search"

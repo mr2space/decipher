aec530117c543fc080c9661bb13a140e5985a50c
Prince Goswami <71509870+mr2space@users.noreply.github.com>
2024-10-22 23:26:02 +0530

    gemini bug fixed

--- backend/src/geminiSDK/gemini.js
+++ backend/src/geminiSDK/gemini.js
@@ -13,7 +13,10 @@ async function GenAiMedicineSuggestion(prompt) {
         });
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
         result = await model.generateContent(prompt);
+        
         //? assumption the result format {"response":{"candidates":[{"content":{"parts":[{"text":"```
+        // console.log(JSON.stringify(result));
+        //? {"response":{"candidates":[{"content":{"parts":[{"text":f
         result =  result.response.text();
         const cleanedString = result.replace(/```json\n|\n```/g, '').trim();
         return JSON.parse(cleanedString);
--- backend/src/geminiSDK/prompt.js
+++ backend/src/geminiSDK/prompt.js
@@ -1,11 +1,11 @@
 const prompt_template = {
     medical: [
         "give the aurvedic remedies for ",
-        ",in format {remedies:[{name, description, benefits, notes}]} , no extra content",
+        ",you should respond as json string format :{remedies:[{name, description, benefits, notes}]} , no extra content",
     ],
     species: [
         "give the aurvedic quality for plant ",
-        ",in format {description, properties, helful_in, benefits} , no extra content"
+        ",you should respond as json string format : {description:String, properties:String, helful_in:String, benefits:String} , no extra content"
     ]
 };
 

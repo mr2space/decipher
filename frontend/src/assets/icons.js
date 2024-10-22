60dabcc63c5cf5a2e1741b0e4cf0dcc377a0e4e0
Rahul Kushwaha <rahul1979kushwaha@gmail.com>
2024-10-23 01:43:11 +0530

    Google map Integration in result page

--- frontend/src/assets/icons.js
+++ frontend/src/assets/icons.js
@@ -1,5 +1,7 @@
 import icon1 from "./images/icon_1.png"
+import icon2 from "./images/icon_2.png"
 
 export default {
     icon1,
+    icon2
 }
--- frontend/src/components/Map/GoogleMapFeature.jsx
+++ frontend/src/components/Map/GoogleMapFeature.jsx
@@ -1,22 +1,45 @@
 import React from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
-const GoogleMap = (props) => {
-  return (
-    <div style={{ width: "100%", height: "400px" }}>
-      <Map
-        google={props.google}
-        style={{ width: "100%", height: "100%" }}
-        zoom={10}
-        initialCenter={{
-          lat: 28.439714,
-          lng: 77.056608,
-        }}
-      />
-    </div>
-  );
+// Style for the container
+const containerStyle = {
+    width: "100%",
+    height: "570px",
 };
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyAQI1u2JMmcusZaJZCq29mv7a4YT9wiLLY"
-})(GoogleMap);
+// Center location (base point)
+const center = {
+    lat: 25.508734,
+    lng: 81.800701,
+};
+
+// Fixed coordinates for markers
+const fixedLocations = [
+    { lat: 25.508734, lng: 81.800701 }, // Point 1
+    { lat: 25.511234, lng: 81.805012 }, // Point 2
+    { lat: 25.506234, lng: 81.803452 }, // Point 3
+    { lat: 25.509984, lng: 81.799134 }, // Point 4
+    { lat: 25.507896, lng: 81.804345 }, // Point 5
+];
+
+const MyGoogleMap = () => {
+    return (
+        <LoadScript googleMapsApiKey="AIzaSyAQI1u2JMmcusZaJZCq29mv7a4YT9wiLLY">
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={11}
+            > 
+                {/* Render fixed markers */}
+                {fixedLocations.map((location, index) => (
+                    <Marker
+                        key={index}
+                        position={location}  
+                    />
+                ))}
+            </GoogleMap>
+        </LoadScript>
+    );
+};
+
+export default MyGoogleMap;

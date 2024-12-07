9f3c90c4940edf7844a973bb0fe16381a0c67f84
Prince Goswami <princegoswami.space@gmail.com>
2024-12-07 22:18:07 +0530

    CHNAGED THE BASE_URL

--- frontend/src/data/UrlData.js
+++ frontend/src/data/UrlData.js
@@ -3,7 +3,7 @@ const URL = {
     OAUTH_URL: "/auth/oauth",
     SIGNUP_URL: "/auth/register",
     HOME_URL : "/",
-    BASE_URL : "http://localhost:80",
+    BASE_URL : "http://localhost:7000",
     REFRESH_URL:"/auth/refresh-token",
     SPECIES_SEARCH_URL : "/search",
     SPECIES_REACT_URL:"/search/result",

5dc833091d798ae5d27ae373f768b435c87ae419
Prince Goswami <princegoswami.space@gmail.com>
2024-12-08 15:27:19 +0530

    updated terraform code

---
+++ Dev/main.tf
@@ -0,0 +1,118 @@
+locals {
+  repository_url = "463470946589.dkr.ecr.us-east-1.amazonaws.com/sanjeevani/backend:latest"
+  env_vars = { for line in split("\n", file("./../backend/.env")) : split("=", line)[0] => split("=", line)[1] if length(trimspace(line)) > 0 && !startswith(trimspace(line), "#") }
+}
+
+provider "aws" {
+  region = "us-east-1" # Replace with your desired region
+}
+
+resource "aws_ecs_cluster" "sanjeevani_cluster" {
+  name = "sanjeevani_cluster" 
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "app-first-task" # Name your task
+  
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "app-first-task",
+      "image": "463470946589.dkr.ecr.us-east-1.amazonaws.com/sanjeevani/backend:latest",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 7000,
+          "hostPort": 7000
+        },
+        {
+        "containerPort": 5173,
+        "hostPort": 5173
+        }
+      ],
+      "memory": 512,
+      "cpu": 256,
+      "environment" : [
+        for key, value in local.env_vars : {
+          "name"  = key
+          "value" = value
+        }
+      ]
+    }
+  ]
+  DEFINITION
+  
+  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory the container requires
+  cpu                      = 256         # Specify the CPU the container requires
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_default_vpc" "default_vpc" {
+}
+
+resource "aws_default_subnet" "default_subnet" {
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_security_group" "ecs_task_security_group" {
+  name_prefix = "ecs-task-sg"
+
+  ingress {
+    from_port   = 7000
+    to_port     = 7000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows access to port 7000 from anywhere
+  }
+
+  ingress {
+    from_port   = 5173
+    to_port     = 5173
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows access to port 5173 from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allows all outbound traffic
+  }
+}
+
+
+resource "aws_ecs_service" "sanjeevani_service" {
+  name            = "sanjeevani-service"
+  cluster         = aws_ecs_cluster.sanjeevani_cluster.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_default_subnet.default_subnet.id]
+    security_groups = [aws_security_group.ecs_task_security_group.id]
+    assign_public_ip = true
+  }
+}

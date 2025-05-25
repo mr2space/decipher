2d4430e7a4bf21f00916c3380ddf1a70c3eb9dde
Rahul Kushwaha <rahul1979kushwaha@gmail.com>
2025-05-26 00:55:56 +0530

    updates on comming soon feature and fixed some bugs

--- frontend/src/components/Auth/FormPage.jsx
+++ frontend/src/components/Auth/FormPage.jsx
@@ -1,6 +1,4 @@
-import React, { Suspense } from "react";
-
-import { Images } from "../../assets";
+import React, { useState, useEffect } from "react";
 
 const image_url_list = [
     "https://sanjeevani-initiate.s3.us-east-1.amazonaws.com/pictures/download+(1).jpg",
@@ -9,13 +7,15 @@ const image_url_list = [
     "https://sanjeevani-initiate.s3.us-east-1.amazonaws.com/pictures/download.jpg",
     "https://sanjeevani-initiate.s3.us-east-1.amazonaws.com/pictures/Forest.jpg",
     "https://sanjeevani-initiate.s3.us-east-1.amazonaws.com/pictures/green+forest.jpg"
-]
-
+];
 
 const FormPage = ({ children }) => {
+    const [imageUrl, setImageUrl] = useState(null);
 
-    // get random image from image_url_list
-    const image_url = image_url_list[Math.floor(Math.random() * image_url_list.length)];
+    useEffect(() => {
+        const randomImage = image_url_list[Math.floor(Math.random() * image_url_list.length)];
+        setImageUrl(randomImage);
+    }, []);
 
     return (
         <section className="py-24 bg-whitegray">
@@ -23,11 +23,13 @@ const FormPage = ({ children }) => {
                 <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-2xl overflow-hidden items-center justify-center gap-10">
                     <div className="w-full lg:w-1/3 flex flex-col items-center justify-center py-8 bg-no-repeat">
                         <div className="flex justify-center items-center ml-3.5 w-full h-full">
-                            <img
-                                src={image_url}
-                                className="rounded-lg object-cover w-full h-full aspect-[4/6]"
-                                alt="plant vector"
-                            />
+                            {imageUrl && (
+                                <img
+                                    src={imageUrl}
+                                    className="rounded-lg object-cover w-full h-full aspect-[4/6]"
+                                    alt="plant"
+                                />
+                            )}
                         </div>
                     </div>
                     <div className="w-full lg:w-1/2 pt-16 pb-8 px-12">
